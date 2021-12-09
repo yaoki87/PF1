3a7a615ca03607ba2c077af73b38a913b069e47a
yaoki87 <878787yao@gmail.com>
2021-12-09 06:55:31 +0000

    ru-to

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #管理者ログイン
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions',
+  passwords: 'admins/passwords',
+  registrations: 'admins/registrations'
+}
+ #トップページ
+ root to: "homes#top"
+
+ #予約
+ resources :resarvations
+ get "/thanx" => "reservations#thanx"
+
+
 end
