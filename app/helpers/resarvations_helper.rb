fad89f39eb089d02949735a914deb6de0d78c727
yaoki87 <878787yao@gmail.com>
2021-12-13 13:49:26 +0000

    devise追加

---
+++ app/helpers/resarvations_helper.rb
@@ -0,0 +1,2 @@
+module ResarvationsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
 
   #管理者ログイン
-  devise_for :admins, controllers: {
-  sessions: 'admins/sessions',
-  passwords: 'admins/passwords',
-  registrations: 'admins/registrations'
-}
- #トップページ
- root to: "homes#top"
+  devise_for :admins
+  
+  #トップページ
+  root "homes#top"
 
- #予約
- resources :resarvations
- get "/thanx" => "reservations#thanx"
+  #予約
+  resources :resarvations
+  get "thanx" => "reservations#thanx"
 
  end
