daed7f667883a8039fb7db7eea85f924433b0fb0
yaoki87 <878787yao@gmail.com>
2022-01-29 08:14:44 +0000

    カレンダー表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -82,3 +82,20 @@ $(function () {
         }
     });
 });
+
+$(function () {
+    function eventCalendar() {
+        return $('#calendar').fullCalendar({});
+    };
+    function clearCalendar() {
+        $('#calendar').html('');
+    };
+    $(document).on('turbolinks:load', function () {
+    eventCalendar();
+    });
+    $(document).on('turbolinks:before-cache', clearCalendar);
+
+    $('#calendar').fullCalendar({
+    events: '/events.json'
+    });
+});
