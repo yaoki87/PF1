5cb4c3b4427f01c1885e90a6debdb36e664a3964
yaoki87 <878787yao@gmail.com>
2022-02-12 06:49:35 +0000

    管理者権限修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -83,19 +83,3 @@ $(function () {
     });
 });
 
-$(function () {
-    function eventCalendar() {
-        return $('#calendar').fullCalendar({});
-    };
-    function clearCalendar() {
-        $('#calendar').html('');
-    };
-    $(document).on('turbolinks:load', function () {
-    eventCalendar();
-    });
-    $(document).on('turbolinks:before-cache', clearCalendar);
-
-    $('#calendar').fullCalendar({
-    events: '/events.json'
-    });
-});
